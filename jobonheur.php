469902208e7142c5fd3459635a17b0ca595cbad9
Liza-maker <lizasamujlova01@gmail.com>
2024-10-04 08:57:50 +0200

    Create jobonheur.php

---
+++ jobonheur.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta http-equiv="Content-Type"
+content="text/html; charset=UTF-8" />
+<link rel="stylesheet" href=
+"jobonheur.css" type="text/css"
+media="screen" />
+<title>JOBONHEUR</title>
+</head>
+<body>
+
+<h1>JOBONHEUR</h1>
+<p>Bienvenue chez <strong>JOBONHEUR</strong> ! Découvrez les offres de travail.</p>
+
+
+</body>
+</html>
